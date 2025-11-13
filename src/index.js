1e132d80c405ef3f9c17060d115aa3a893d4aed1
Adel <Adel@gmail.com>
2025-11-13 10:51:46 +0200

    Add new additions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./pages/home";
+import HTML from "./pages/html";
+import Css from "./pages/css";
+import Javascript from "./pages/javascript";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1>Error 404 - Page Not Found</h1>,
+  },
+  {
+    path: "/html",
+    element: <HTML />,
+  },
+  {
+    path: "/css",
+    element: <Css />,
+  },
+  {
+    path: "/javascript",
+    element: <Javascript />,
+  },
+]);
+
+const root = createRoot(document.getElementById("root"));
+
+// تشغيل الراوتر
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
